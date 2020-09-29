b026cb9202e7c8190284c2f6520cea5d5e30461c
Neil John Medalla <medallaneiljohn@gmail.com>
2020-09-28 18:46:36 -0600

    added auth and http services

---
+++ src/services/authService.js
@@ -0,0 +1,44 @@
+import jwtDecode from "jwt-decode";
+import http from "./httpService";
+import { authReceived } from "../store/auth";
+
+const apiEndpoint = "/auth";
+const tokenKey = "token";
+
+http.setJwt(getJwt());
+
+export const login = async (email, password) => {
+  const { data: jwt } = await http.post(apiEndpoint, { email, password });
+  localStorage.setItem(tokenKey, jwt);
+};
+
+export const logout = () => {
+  localStorage.removeItem(tokenKey);
+  http.setJwt(false);
+};
+
+export const getCurrentUser = () => {
+  try {
+    const jwt = localStorage.getItem(tokenKey);
+    return jwtDecode(jwt);
+  } catch (error) {
+    return null;
+  }
+};
+
+// used to set user current user data everytime page refresh if user is already authenticated
+export const setCurrentUser = (store) => {
+  if (localStorage.token) store.dispatch(authReceived(getCurrentUser()));
+};
+
+export function getJwt() {
+  return localStorage.getItem(tokenKey);
+}
+
+export default {
+  login,
+  logout,
+  getCurrentUser,
+  getJwt,
+  setCurrentUser,
+};
---
+++ src/services/httpService.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+
+axios.defaults.baseURL = "http://localhost:5000/api";
+
+axios.interceptors.response.use(null, (error) => {
+  const expectedError =
+    error.response && error.response >= 400 && error.response < 500;
+
+  if (!expectedError) {
+    console.log(error);
+    toast.error("an unexpected error occurred");
+  }
+
+  return Promise.reject(error);
+});
+
+export const setJwt = (jwt) =>
+  (axios.defaults.headers.common["x-auth-token"] = jwt);
+
+export default {
+  get: axios.get,
+  post: axios.post,
+  put: axios.put,
+  delete: axios.delete,
+  setJwt,
+};
