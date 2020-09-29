b1d6aca052fd733595063182c32ccc5cd116eee4
Neil John Medalla <medallaneiljohn@gmail.com>
2020-09-29 00:26:35 -0600

    moved navbar comp to app and import styles.css

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ import configureStore from "./store/configureStore";
 import Login from "./components/login";
 import auth from "./services/authService";
 import PrivateRoute from "./components/common/privateRoute";
-
+import BookAppointmentForm from "./components/bookAppoinmentForm";
+import NavBar from "./components/common/navBar";
+import "react-datepicker/dist/react-datepicker.css";
+import "./styles.css";
 const store = configureStore();
 
 auth.setCurrentUser(store);
@@ -21,9 +24,11 @@ function App() {
     <Provider store={store}>
       <div className="App">
         <ToastContainer />
+        <NavBar />
         <Router>
           <Login path="/" />
           <PrivateRoute as={AppointmentsTable} path="/dashboard" user={user} />
+          <PrivateRoute as={BookAppointmentForm} path="/book-appointment" />
         </Router>
       </div>
     </Provider>
