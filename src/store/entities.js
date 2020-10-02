3633ca59ba2744f45ed59cb58ad9981bd07a2e3f
Neil John Medalla <medallaneiljohn@gmail.com>
2020-10-02 04:11:09 -0600

    imported users reducer

--- src/store/entities.js
+++ src/store/entities.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import appointmentsReducer from "./appointments";
 import authReducer from "./auth";
+import usersReducer from "./users";
 
 export default combineReducers({
   appointments: appointmentsReducer,
   auth: authReducer,
+  users: usersReducer,
 });
