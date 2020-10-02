e554ce79006cb327ffd3f23e038e1b4b68a32777
Neil John Medalla <medallaneiljohn@gmail.com>
2020-10-02 04:08:58 -0600

    added new action type

--- src/store/appointments.js
+++ src/store/appointments.js
@@ -11,20 +11,33 @@ const slice = createSlice({
     appointmentsReceived: (appointments, action) => {
       appointments.list = action.payload;
     },
+    appointmentAdded: (appointments, action) => {
+      appointments.list.push(action.payload);
+    },
   },
 });
 
-const { appointmentsReceived } = slice.actions;
+const { appointmentsReceived, appointmentAdded } = slice.actions;
 export default slice.reducer;
-
+const url = "/bookings";
 export const loadAppointments = () => (dispatch, getState) => {
   return dispatch(
     apiCallBegan({
-      url: "/bookings",
+      url,
       method: "GET",
       onSuccess: appointmentsReceived.type,
     })
   );
+};
+
+export const addAppointment = (appointment) => {
+  appointment.date = appointment.date.getTime();
+  return apiCallBegan({
+    url,
+    method: "POST",
+    data: appointment,
+    onSuccess: appointmentAdded.type,
+  });
 };
 
 export const getAppointments = createSelector(
