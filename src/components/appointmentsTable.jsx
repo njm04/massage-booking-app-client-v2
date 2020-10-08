4f68a794f46b1a4f535c0103fe874a02dc1ec000
Neil John Medalla <medallaneiljohn@gmail.com>
2020-10-08 03:38:11 -0600

    added form modal, labels and paths

--- src/components/appointmentsTable.jsx
+++ src/components/appointmentsTable.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 // import { Table } from "react-bootstrap";
 import { loadAppointments, getAppointments } from "../store/appointments";
@@ -7,22 +7,32 @@ import { Button } from "react-bootstrap";
 // import TableHeader from "./common/tableHeader";
 // import TableBody from "./common/tableBody";
 import Table from "./common/table";
+import FormModal from "./formModal";
 
 const AppointmentsTable = () => {
   const dispatch = useDispatch();
   const appointments = useSelector(getAppointments);
+  const [appointmentId, setAppointmentId] = useState("");
+  const [show, setShow] = useState(false);
+
+  // data came from tableBody and passed to content() then to handleShow
+  const handleShow = (id) => {
+    setShow(true);
+    setAppointmentId(id);
+  };
 
   const columns = [
     { label: "Name", path: "name" },
     { label: "Massage type", path: "massageType" },
     { label: "Duration", path: "duration" },
+    { label: "Therapist", path: "therapistName" },
     { label: "Contact number", path: "contactNumber" },
     { label: "Address", path: "address" },
     { label: "Date", path: "date" },
     {
       key: "edit",
-      content: () => (
-        <Button variant="outline-info">
+      content: (id) => (
+        <Button variant="outline-info" onClick={() => handleShow(id)}>
           <FaEdit />
         </Button>
       ),
@@ -49,6 +59,7 @@ const AppointmentsTable = () => {
         <TableHeader columns={columns} />
         <TableBody columns={columns} data={appointments} />
       </Table> */}
+      <FormModal show={show} setShow={setShow} appointmentId={appointmentId} />
     </>
   );
 };
