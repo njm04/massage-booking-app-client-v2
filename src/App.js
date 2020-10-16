f46b7d70a73c74956b02d3935e3d6b1902f11fa6
Neil John Medalla <medallaneiljohn@gmail.com>
2020-10-15 22:34:29 -0600

    moved auth and store to index.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import "./App.css";
-import { Provider } from "react-redux";
 import { Router } from "@reach/router";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import AppointmentsTable from "./components/appointmentsTable";
-import configureStore from "./store/configureStore";
 import Login from "./components/login";
-import auth from "./services/authService";
 import PrivateRoute from "./components/common/privateRoute";
 import BookAppointmentForm from "./components/bookAppoinmentForm";
 import SchedulesCalendar from "./components/calendar";
@@ -16,27 +13,19 @@ import "react-datepicker/dist/react-datepicker.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import "./styles.css";
 
-const store = configureStore();
-
-auth.setCurrentUser(store);
-
 function App() {
-  const user = auth.getCurrentUser();
-  console.log(user);
   return (
-    <Provider store={store}>
-      <div className="App">
-        <ToastContainer />
-        <Router>
-          <Login path="/login" />
-          <PrivateRoute as={Dashboard} path="/">
-            <PrivateRoute as={AppointmentsTable} path="/" />
-            <PrivateRoute as={BookAppointmentForm} path="/book-appointment" />
-            <PrivateRoute as={SchedulesCalendar} path="/calendar" />
-          </PrivateRoute>
-        </Router>
-      </div>
-    </Provider>
+    <div className="App">
+      <ToastContainer />
+      <Router>
+        <Login path="/login" />
+        <PrivateRoute as={Dashboard} path="/">
+          <PrivateRoute as={AppointmentsTable} path="/" />
+          <PrivateRoute as={BookAppointmentForm} path="/book-appointment" />
+          <PrivateRoute as={SchedulesCalendar} path="/calendar" />
+        </PrivateRoute>
+      </Router>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
+import configureStore from "./store/configureStore";
+import auth from "./services/authService";
+
+const store = configureStore();
+auth.setCurrentUser(store);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
