3123aa39788d05faca9489c90b08f5c6b2f767ba
Neil John Medalla <medallaneiljohn@gmail.com>
2020-10-20 05:19:27 -0600

    created birthDatePicker component

---
+++ src/components/common/birthDatePicker.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+
+const BirthDatePicker = ({ value, onChange }) => {
+  // const [startDate, setStartDate] = useState(new Date());
+  return (
+    <DatePicker
+      className="form-control"
+      placeholderText="Select birthdate"
+      selected={value}
+      onChange={(e) => {
+        onChange(e);
+      }}
+      peekNextMonth
+      showMonthDropdown
+      showYearDropdown
+      yearDropdownItemNumber={2}
+      dropdownMode="select"
+    />
+  );
+};
+
+export default BirthDatePicker;
