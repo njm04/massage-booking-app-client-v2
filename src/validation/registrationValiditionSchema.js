aa6b4c3af87738c9c119cd6123fe0de92e28064a
Neil John Medalla <medallaneiljohn@gmail.com>
2020-10-22 01:26:00 -0600

    added min max password validation

--- src/validation/registrationValiditionSchema.js
+++ src/validation/registrationValiditionSchema.js
@@ -5,7 +5,11 @@ export const registrationValiditionSchema = yup.object().shape({
     .string()
     .email("Invalid email address")
     .required("Email address is a required field"),
-  password: yup.string().required("Password is a required field"),
+  password: yup
+    .string()
+    .min(5)
+    .max(1000)
+    .required("Password is a required field"),
   firstName: yup.string().required("First name is a required field"),
   lastName: yup.string().required("Last name is a required field"),
   gender: yup.string().required("Gender is a required field"),
