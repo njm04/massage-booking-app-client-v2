5fbf8dd073bb52724e233ecae94ee4b986811ace
Neil John Medalla <medallaneiljohn@gmail.com>
2020-10-28 07:01:43 -0600

    form conditional rendering depending on user type

--- src/components/formModal.jsx
+++ src/components/formModal.jsx
@@ -1,15 +1,47 @@
 import React, { useRef } from "react";
 import { Button, Modal } from "react-bootstrap";
 import EditAppointmentForm from "./editAppointmentForm";
+import UpdateAppointmentStatusModal from "./updateAppointmentStatusForm";
 import Spinner from "./common/spinner";
+import { getUser } from "../store/auth";
+import { useSelector } from "react-redux";
 
 const FormModal = ({ show, setShow, appointmentId, therapists, loading }) => {
   const formRef = useRef();
-  const handleClose = () => setShow(false);
+  const user = useSelector(getUser);
 
+  const handleClose = () => setShow(false);
   const handleSubmit = (e) => {
     e.preventDefault();
     formRef.current.submit();
+  };
+
+  const form = () => {
+    if (user && user.userType && user.userType.name === "therapist") {
+      return (
+        <UpdateAppointmentStatusModal
+          ref={formRef}
+          appointmentId={appointmentId}
+          therapists={therapists}
+        />
+      );
+    } else {
+      return (
+        <EditAppointmentForm
+          ref={formRef}
+          appointmentId={appointmentId}
+          therapists={therapists}
+        />
+      );
+    }
+  };
+
+  const title = () => {
+    if (user && user.userType && user.userType.name === "therapist") {
+      return "Update Status";
+    } else {
+      return "Edit Appointment";
+    }
   };
 
   return (
@@ -22,15 +54,9 @@ const FormModal = ({ show, setShow, appointmentId, therapists, loading }) => {
         size="xl"
       >
         <Modal.Header closeButton>
-          <Modal.Title>Edit Appointment</Modal.Title>
+          <Modal.Title>{title()}</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
-          <EditAppointmentForm
-            ref={formRef}
-            appointmentId={appointmentId}
-            therapists={therapists}
-          />
-        </Modal.Body>
+        <Modal.Body>{form()}</Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose} disabled={loading}>
             Close
