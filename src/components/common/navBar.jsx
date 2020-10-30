4ba1652e14691259dab8bb20b01910987a281045
Neil John Medalla <medallaneiljohn@gmail.com>
2020-10-30 05:19:42 -0600

    created dropdown nav

--- src/components/common/navBar.jsx
+++ src/components/common/navBar.jsx
@@ -1,60 +1,74 @@
-import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
-import Logout from "../common/logout";
+import React, { useState } from "react";
+import { Navbar, Nav, Dropdown } from "react-bootstrap";
 import { getUser } from "../../store/auth";
 import { useSelector } from "react-redux";
+import { FaUserCog } from "react-icons/fa";
+import Logout from "../common/logout";
 import NavLink from "./navLink";
+import ChangePasswordButton from "../changePasswordButton";
+import ChangePasswordModal from "../changePasswordModal";
 
 const NavBar = () => {
   const user = useSelector(getUser);
   const { firstName, lastName, userType = {} } = user;
+  const [show, setShow] = useState(false);
 
   return (
-    <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="/">Massage Clinic</Navbar.Brand>
-      <Nav className="mr-auto">
-        <NavLink to="/" className="nav-link">
-          Appointments
-        </NavLink>
-        {user && userType.name === "customer" ? (
-          <NavLink className="nav-link" to="/book-appointment">
-            Book Appointment
-          </NavLink>
-        ) : null}
-        {user && userType.name === "therapist" ? (
-          <NavLink className="nav-link" to="/calendar">
-            Calendar
-          </NavLink>
-        ) : null}
-        {user && userType.name === "admin" ? (
-          <>
-            <NavLink className="nav-link" to="/book-appointment">
-              Book Appointment
+    <>
+      <Navbar bg="dark" variant="dark" expand="lg" className="mx-auto">
+        <Navbar.Brand href="/">Massage Clinic</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <NavLink to="/" className="nav-link">
+              Appointments
             </NavLink>
-            <NavLink className="nav-link" to="/calendar">
-              Calendar
-            </NavLink>
-            <NavLink className="nav-link" to="/users">
-              Users
-            </NavLink>
-            <NavLink className="nav-link" to="/create-user">
-              Create User
-            </NavLink>
-          </>
-        ) : null}
-      </Nav>
-      {/* <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-info">Search</Button>
-    </Form> */}
-      <Navbar.Text className="mr-2">
-        Signed in as:{" "}
-        <a href="#login">
-          {firstName} {lastName}
-        </a>
-      </Navbar.Text>
-      <Logout />
-    </Navbar>
+            {user && userType.name === "customer" ? (
+              <NavLink className="nav-link" to="/book-appointment">
+                Book Appointment
+              </NavLink>
+            ) : null}
+            {user && userType.name === "therapist" ? (
+              <NavLink className="nav-link" to="/calendar">
+                Calendar
+              </NavLink>
+            ) : null}
+            {user && userType.name === "admin" ? (
+              <>
+                <NavLink className="nav-link" to="/book-appointment">
+                  Book Appointment
+                </NavLink>
+                <NavLink className="nav-link" to="/calendar">
+                  Calendar
+                </NavLink>
+                <NavLink className="nav-link" to="/users">
+                  Users
+                </NavLink>
+                <NavLink className="nav-link" to="/create-user">
+                  Create User
+                </NavLink>
+              </>
+            ) : null}
+          </Nav>
+          <Navbar.Text className="mr-2">
+            Signed in as:{" "}
+            <a href="#login">
+              {firstName} {lastName}
+            </a>
+          </Navbar.Text>
+          <Dropdown>
+            <Dropdown.Toggle variant="outline-light" id="dropdown-basic">
+              <FaUserCog />
+            </Dropdown.Toggle>
+            <Dropdown.Menu className="dropdown-menu-right">
+              <Logout />
+              <ChangePasswordButton setShow={setShow} />
+            </Dropdown.Menu>
+          </Dropdown>
+        </Navbar.Collapse>
+      </Navbar>
+      <ChangePasswordModal show={show} setShow={setShow} />
+    </>
   );
 };
 
