f34fd986d366909975782d41c8af9da466dcfcaa
Neil John Medalla <medallaneiljohn@gmail.com>
2020-11-07 03:09:49 -0700

    implemented search and pagination

--- src/components/usersTable.jsx
+++ src/components/usersTable.jsx
@@ -9,10 +9,13 @@ import {
   isLoading,
 } from "../store/users";
 import { getUser } from "../store/auth";
+import { paginate } from "../utils/paginate";
 import { FaTrashAlt, FaEdit } from "react-icons/fa";
 import { Button, Spinner, Badge } from "react-bootstrap";
 import Table from "./common/table";
 import EditUserModal from "./editUserModal";
+import SearchBox from "./common/searchBox";
+import Pagination from "./common/pagination";
 
 const UsersTable = () => {
   const dispatch = useDispatch();
@@ -22,6 +25,9 @@ const UsersTable = () => {
   const userById = useSelector(getUserById);
   const [show, setShow] = useState(false);
   const [user, setUser] = useState({});
+  const [searchQuery, setSearchQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize] = useState(10);
 
   const handleShow = (id) => {
     setShow(true);
@@ -64,6 +70,31 @@ const UsersTable = () => {
     },
   ];
 
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const getPageData = () => {
+    let filtered = [];
+    if (searchQuery) {
+      filtered = users.filter(
+        (user) =>
+          user.name.toLowerCase().indexOf(searchQuery) !== -1 ||
+          user.userType.toLowerCase().indexOf(searchQuery) !== -1 ||
+          user.status.toLowerCase().indexOf(searchQuery) !== -1
+      );
+    } else {
+      filtered = users;
+    }
+    const paginatedUsers = paginate(filtered, currentPage, pageSize);
+
+    return { totalCount: filtered.length, data: paginatedUsers };
+  };
+
   useEffect(() => {
     dispatch(loadUsers());
   }, [dispatch]);
@@ -78,6 +109,11 @@ const UsersTable = () => {
 
   return (
     <>
+      <SearchBox
+        value={searchQuery}
+        onChange={handleSearch}
+        placeholder="Search (name, user type, status)"
+      />
       {loading ? (
         <div>
           <Table columns={columns} />
@@ -85,8 +121,14 @@ const UsersTable = () => {
           <h2>Loading...</h2>
         </div>
       ) : (
-        <Table columns={columns} data={users} />
+        <Table columns={columns} data={getPageData().data} />
       )}
+      <Pagination
+        itemsCount={getPageData().totalCount}
+        pageSize={pageSize}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />
       <EditUserModal
         show={show}
         setShow={setShow}
