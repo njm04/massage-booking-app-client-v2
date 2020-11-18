db85daaf2a0209058956fae396c78e255b38eeb9
Neil John Medalla <medallaneiljohn@gmail.com>
2020-11-17 17:04:16 -0700

    refactored login implementation

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -3,11 +3,12 @@ import { Form, Button, Card, Alert } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers";
 import { navigate, Redirect } from "@reach/router";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import auth from "../services/authService";
 import http from "../services/httpService";
 import Input from "./common/input";
-import { authReceived } from "../store/auth";
+import Spinner from "./common/spinner";
+import { authReceived, login, isLoading } from "../store/auth";
 import { loginValidationSchema } from "../validation/validationSchemas";
 
 const Login = () => {
@@ -15,9 +16,10 @@ const Login = () => {
     resolver: yupResolver(loginValidationSchema),
   });
   const dispatch = useDispatch();
+  const loading = useSelector(isLoading);
 
   const onSubmit = async (data) => {
-    await auth.login(data.email, data.password);
+    await dispatch(login(data));
     http.setJwt(auth.getJwt());
     dispatch(authReceived(auth.getCurrentUser()));
     navigate("/");
@@ -31,52 +33,63 @@ const Login = () => {
   return (
     <div>
       <Form className="mt-3" onSubmit={handleSubmit(onSubmit)}>
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-lg-6">
-              <Card className="mt-3">
-                <Card.Header as="h2">Login</Card.Header>
-                <Card.Body>
-                  <Form.Group controlId="formBasicEmail">
-                    <Input
-                      name="email"
-                      label="Email"
-                      placeholder="Enter email"
-                      register={register}
-                    />
-                    {errors.email && (
-                      <Alert variant="danger">{errors.email.message}</Alert>
-                    )}
-                  </Form.Group>
+        <fieldset disabled={loading}>
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-lg-6">
+                <Card className="mt-3">
+                  <Card.Header as="h2">Login</Card.Header>
+                  <Card.Body>
+                    <Form.Group controlId="formBasicEmail">
+                      <Input
+                        name="email"
+                        label="Email"
+                        placeholder="Enter email"
+                        register={register}
+                      />
+                      {errors.email && (
+                        <Alert variant="danger">{errors.email.message}</Alert>
+                      )}
+                    </Form.Group>
 
-                  <Form.Group controlId="formBasicPassword">
-                    <Input
-                      name="password"
-                      label="Password"
-                      placeholder="Password"
-                      register={register}
-                      type="password"
-                    />
-                    {errors.password && (
-                      <Alert variant="danger">{errors.password.message}</Alert>
-                    )}
-                  </Form.Group>
-                  <Button variant="primary" type="submit" block>
-                    login
-                  </Button>
-                  <Button
-                    variant="success"
-                    type="button"
-                    onClick={onClick}
-                    block
-                  >
-                    Create Account
-                  </Button>
-                </Card.Body>
-              </Card>
+                    <Form.Group controlId="formBasicPassword">
+                      <Input
+                        name="password"
+                        label="Password"
+                        placeholder="Password"
+                        register={register}
+                        type="password"
+                      />
+                      {errors.password && (
+                        <Alert variant="danger">
+                          {errors.password.message}
+                        </Alert>
+                      )}
+                    </Form.Group>
+                    <Button variant="primary" type="submit" block>
+                      {loading ? (
+                        <>
+                          <Spinner />
+                          <span>logging in</span>
+                        </>
+                      ) : (
+                        <span>login</span>
+                      )}
+                    </Button>
+                    <Button
+                      variant="success"
+                      type="button"
+                      onClick={onClick}
+                      block
+                    >
+                      Create Account
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </div>
             </div>
           </div>
-        </div>
+        </fieldset>
       </Form>
     </div>
   );
--- src/store/auth.js
+++ src/store/auth.js
@@ -1,14 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { createSelector } from "reselect";
 import { isEmpty } from "lodash";
+import { apiCallBegan } from "./api";
+
+const url = "/auth";
+const tokenKey = "token";
 
 const slice = createSlice({
   name: "auth",
   initialState: {
     user: {},
     isAuthenticated: false,
+    loading: false,
   },
   reducers: {
+    authRequested: (auth, action) => {
+      auth.loading = true;
+    },
+    authFailed: (auth, action) => {
+      auth.loading = false;
+    },
+    authTokenReceived: (auth, action) => {
+      localStorage.setItem(tokenKey, action.payload);
+      auth.loading = false;
+    },
     authReceived: (auth, action) => {
       auth.user = action.payload;
       auth.isAuthenticated = !isEmpty(action.payload);
@@ -20,11 +35,28 @@ const slice = createSlice({
   },
 });
 
+const { authRequested, authFailed, authTokenReceived } = slice.actions;
 export const { authReceived, userLoggedOut } = slice.actions;
 export default slice.reducer;
+
+export const login = (user) => {
+  return apiCallBegan({
+    url,
+    method: "POST",
+    data: user,
+    onStart: authRequested.type,
+    onSuccess: authTokenReceived.type,
+    onFailure: authFailed.type,
+  });
+};
 
 export const getUser = createSelector(
   (state) => state.entities.auth,
   // check isAuthenticated or not, this function work either way
   (auth) => (auth.isAuthenticated ? auth.user : {})
+);
+
+export const isLoading = createSelector(
+  (state) => state.entities.auth,
+  (auth) => auth.loading
 );
